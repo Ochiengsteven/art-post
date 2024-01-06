981d9d3e71eba24f89261484e31309067dad87be
Ochiengsteven <stevenochieng6305@gmail.com>
2024-01-06 20:18:26 +0300

    CREATE TABLES AND RELATIONSHIPS

---
+++ db/migrate/20240106165245_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.integer :author_id
+      t.string :title
+      t.text :text
+      t.integer :likes_counter, default: 0
+      t.integer :comments_counter, default: 0
+
+      t.timestamps
+    end
+    add_index :posts, :author_id
+  end
+end
