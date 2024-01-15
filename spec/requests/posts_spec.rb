17fae642f0f6fd113bb7681cb8987a3bb245b687
Ochiengsteven <stevenochieng6305@gmail.com>
2024-01-15 19:31:50 +0300

    FIX LINTERS

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,37 +4,37 @@ RSpec.describe 'Posts', type: :request do
   let(:user) { User.create(name: 'John', photo: 'https://example.com/john.jpg', bio: 'Lorem ipsum') }
   let(:post) { Post.create(author: user, title: 'Test Post', text: 'This is a test post') }
 
-  describe "GET /users/:user_id/posts" do
-    it "returns a successful response" do
+  describe 'GET /users/:user_id/posts' do
+    it 'returns a successful response' do
       get user_posts_path(user)
       expect(response).to have_http_status(200)
     end
 
-    it "renders the index template" do
+    it 'renders the index template' do
       get user_posts_path(user)
       expect(response).to render_template(:index)
     end
 
-    it "includes correct placeholder text in the response body" do
+    it 'includes correct placeholder text in the response body' do
       get user_posts_path(user)
-      expect(response.body).to include("All posts")
+      expect(response.body).to include('All posts')
     end
   end
 
-  describe "GET /posts/:id" do
-    it "returns a successful response" do
+  describe 'GET /posts/:id' do
+    it 'returns a successful response' do
       get post_path(post)
       expect(response).to have_http_status(200)
     end
 
-    it "renders the show template" do
+    it 'renders the show template' do
       get post_path(post)
       expect(response).to render_template(:show)
     end
 
-    it "includes correct placeholder text in the response body" do
+    it 'includes correct placeholder text in the response body' do
       get post_path(post)
-      expect(response.body).to include("Post Details")
+      expect(response.body).to include('Post Details')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,37 +3,37 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   let(:user) { User.create(name: 'John', photo: 'https://example.com/john.jpg', bio: 'Lorem ipsum') }
 
-  describe "GET /users" do
-    it "returns a successful response" do
+  describe 'GET /users' do
+    it 'returns a successful response' do
       get users_path
       expect(response).to have_http_status(200)
     end
 
-    it "renders the index template" do
+    it 'renders the index template' do
       get users_path
       expect(response).to render_template(:index)
     end
 
-    it "includes correct placeholder text in the response body" do
+    it 'includes correct placeholder text in the response body' do
       get users_path
-      expect(response.body).to include("All Users")
+      expect(response.body).to include('All Users')
     end
   end
 
-  describe "GET /users/:id" do
-    it "returns a successful response" do
+  describe 'GET /users/:id' do
+    it 'returns a successful response' do
       get user_path(user)
       expect(response).to have_http_status(200)
     end
 
-    it "renders the show template" do
+    it 'renders the show template' do
       get user_path(user)
       expect(response).to render_template(:show)
     end
 
-    it "includes correct placeholder text in the response body" do
+    it 'includes correct placeholder text in the response body' do
       get user_path(user)
-      expect(response.body).to include("List of posts for a User")
+      expect(response.body).to include('List of posts for a User')
     end
   end
 end
