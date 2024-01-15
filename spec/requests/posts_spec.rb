cb9aef9e88c88ed971e088120adee56180665f10
Ochiengsteven <stevenochieng6305@gmail.com>
2024-01-15 19:28:23 +0300

    add controller specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
+  let(:user) { User.create(name: 'John', photo: 'https://example.com/john.jpg', bio: 'Lorem ipsum') }
+  let(:post) { Post.create(author: user, title: 'Test Post', text: 'This is a test post') }
+
+  describe "GET /users/:user_id/posts" do
+    it "returns a successful response" do
+      get user_posts_path(user)
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get user_posts_path(user)
+      expect(response).to render_template(:index)
+    end
+
+    it "includes correct placeholder text in the response body" do
+      get user_posts_path(user)
+      expect(response.body).to include("All posts")
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
-      expect(response).to have_http_status(:success)
+  describe "GET /posts/:id" do
+    it "returns a successful response" do
+      get post_path(post)
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the show template" do
+      get post_path(post)
+      expect(response).to render_template(:show)
+    end
+
+    it "includes correct placeholder text in the response body" do
+      get post_path(post)
+      expect(response.body).to include("Post Details")
     end
   end
 end
