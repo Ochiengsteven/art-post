ad8d188525aba0f54dae672129a694a5f02e0445
Ochiengsteven <stevenochieng6305@gmail.com>
2024-01-16 09:39:34 +0300

    ADD POST VIEW FILES

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,15 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+    @recent_comments = {}
+
+    @posts.each do |post|
+      @recent_comments[post.id] = post.recent_comments
+    end
   end
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 end
