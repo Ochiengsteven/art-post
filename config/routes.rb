f72b9b17b025562848ddfa1d1d182c6307c5d567
M.Nisar Ali <dr.nisaralig@gmail.com>
2024-01-24 17:24:11 +0500

    Add the login path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # routes.rb
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   root "users#index"
 
   resources :users, only: [:index, :show] do
