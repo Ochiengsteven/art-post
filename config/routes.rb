de655073cbd464f3c79b8a04a0a8295cea06530e
Ochiengsteven <stevenochieng6305@gmail.com>
2024-01-27 13:02:18 +0300

    ADD COMMENTS END POINTS

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,17 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :destroy]
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index]
+      end
+
+      resources :posts do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
