65520224a75c837ef33fad9e354ddee17d400b44
Ochiengsteven <stevenochieng6305@gmail.com>
2024-01-27 21:36:28 +0300

    ADD COMMENTS CONTROLLER

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,37 +1,38 @@
 class Api::V1::CommentsController < ApplicationController
   before_action :set_post, only: %i[index create]
   skip_before_action :verify_authenticity_token
-
+  
   # GET /api/v1/users/:user_id/posts/:post_id/comments
   def index
-    @comments = @post.comments
-    render json: @comments
+  @comments = @post.comments
+  render json: @comments
   end
-
+  
   # POST /api/v1/posts/:post_id/comments
-
+  
   def create
-    if current_user.nil?
-      render json: { errors: ['Authentication required'] }, status: :unauthorized
-      return
-    end
-    @comment = @post.comments.create(comment_params.merge(user: current_user))
-
-    if @comment.save
-      render json: @comment, status: :created
-    else
-      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
-
-    end
+  if current_user.nil?
+  render json: { errors: ['Authentication required'] }, status: :unauthorized
+  return
   end
-
+  @comment = @post.comments.create(comment_params.merge(user: current_user))
+  
+  if @comment.save
+  render json: @comment, status: :created
+  else
+  render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+  
+  end
+  end
+  
   private
-
+  
   def set_post
-    @post = Post.find(params[:post_id])
+  @post = Post.find(params[:post_id])
   end
-
+  
   def comment_params
-    params.require(:comment).permit(:text)
+  params.require(:comment).permit(:text)
   end
-end
+  end
+  
